2df3c4d23c9d71ef87fbfa6b906c064d6331fb3b
Keila19JH <keila19gutierrez@gmail.com>
2023-09-05 20:10:56 -0600

    Ejercicios de práctica sobre condicionales

--- src/main.ts
+++ src/main.ts
@@ -1,75 +1,38 @@
 import './style.css'
 
-// Basic Structure if(){}
-// Operadores de condicion >, <, >=, <=, ==, !=
-// PROPOSICIÓN QUE SE TIENE QUE CUMPLIR para condicionales || and && : || (se puede cumplir siempre y cuando una condición que se cumpla basta), 
-// && (se tiene que cumplir toda la condición).
+let cadena: string = 'Hola, Mundo'; 
 
-
-// Basic
-
-let ifExist: boolean = true;
-let number: number = 10;
-
-if( ifExist ){
-  console.log('Si existe');
-}
-
-//if( number > 10 && number < 15 ){
-//  console.log('Esta en el rango');
-//}
-
-if( number > 10 || (number < 15 && number > 0) ){
-  console.log('Esta en el rango');
-}
-// else
-let edad: number = 20;
-
-if( edad > 60 ){
-  console.log('Abuelo');
+///
+if(cadena){
+    console.log('La cadena esta llena' )
 }
 else{
-  console.log('joven');
+  console.log('La cadena esta vacia')
 }
 
-// NESTED CONDITIONAL (IF ANIDADOS)
-let number1: number = 30;
-let number2: number = 31;
-let number3: number = 100;
+///
+console.log('La longitud de la cadena es de: ' + cadena.length);
 
-// What´s the max number?
-if( number1 > number2 && number1 > number3){
-  console.log(`El número mayor es: ${number1}`);
+//console.log('¿La palabra mundo esta incluida en el texto?: ' + cadena.includes("Mundo"));
+
+///
+if(cadena.includes('Mundo')){
+  console.log('La cadena incluye: ' + cadena);
 }
-else{
-  if(number2 > number1 && number2 > number3){
-    console.log(`El número mayor es: ${number2}`);
-  }
   else{
-  console.log(`El número mayor es: ${number3}`);
+    console.log('La cadena incluye: ' + cadena);
   }
-}
 
-// TERNARY CONDITIONAL (Trabaja con código de una sola linea)
-let message: string = 'not authenticated';
+///
+console.log(cadena.toUpperCase());
 
-// si cumple se pone un ?, si no se cumple se pone un :
-(message === 'authenticated')
-  ? console.log('usario autenticado')
-  : console.log('usario denegado');
+///
+//console.log(cadena.split('')[0]);
+console.log(cadena [cadena.length - 1]);
 
-// Solo es por si se tiene que cumplir, no muestra más ni ejecuta más
-(message === 'authenticated')
-  && console.log('usario autenticado');
+///
+console.log(cadena.substring(11));
 
-// Exactamente igual(===) e igual (==)
-let num1: number = 8, num2: string = '8';
-
-// + o number (para convertir un string a number)
-if ( num1 === +num2){
-  console.log('Si es igual');
-
-}
 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
